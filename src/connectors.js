6e5f06d3c7720a80ced55e6f1eb9518a27b0cdb4
TungNT <tungnt@gviet.vn>
2022-01-26 10:16:53 +0700

    Update

--- src/connectors.js
+++ src/connectors.js
@@ -1,4 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 56, 97, 137, 1337, 43114, 80001],
+  // supportedChainIds: [1, 3, 4, 5, 42, 56, 97, 137, 1337, 43114, 80001],
+  supportedChainIds: [97],
 });
